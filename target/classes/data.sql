81855371bd7558777556562b7293ef5e45f30354
xr-chen <56777910+xr-chen@users.noreply.github.com>
2022-11-20 22:51:13 -0600

    database setup

---
+++ target/classes/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO PRODUCT (PRODUCT_NAME, PRODUCT_TYPE, PRICE, QUANTITY) VALUES
+    ('Deep work', 'Book', 14.99, 249),
+    ('Introduction to Pandas', 'Book', 100.22, 298),
+    ('Python Machine Learning Handbook', 'Book', 99.00, 234),
+    ('Statistical Inference', 'Book', 99.00, 2334),
+    ('Fellow Ode', 'Coffee Grinder', 299.00, 10);
