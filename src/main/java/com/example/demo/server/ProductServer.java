20e65da37bc48fc5e5c80cd9dc16992003b53b37
xr-chen <56777910+xr-chen@users.noreply.github.com>
2023-04-04 22:10:11 -0500

    Add Jenkins file

--- src/main/java/com/example/demo/server/ProductServer.java
+++ src/main/java/com/example/demo/server/ProductServer.java
@@ -1,17 +1,43 @@
 package com.example.demo.server;
 
+import com.example.demo.dao.ProductRepository;
 import com.example.demo.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface ProductServer {
-    Product findById(long id);
+@Service
+public class ProductServerImple implements ProductServer {
 
-    Product saveProduct(Product product);
+    private final ProductRepository productRepository;
 
-    Product updateProduct(Product product);
+    @Autowired
+    ProductServerImple(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+    @Override
+    public Product findById(long id) {
+        return productRepository.findById(id).orElse(null);
+    }
 
-    void deleteProductById(long id);
+    @Override
+    public Product saveProduct(Product product) {
+        return productRepository.save(new Product(product.getName(), product.getType(), product.getPrice(), product.getQuantity()));
+    }
 
-    List<Product> findAllProducts();
+    @Override
+    public Product updateProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public void deleteProductById(long id) {
+        productRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Product> findAllProducts() {
+        return (List<Product>) productRepository.findAll();
+    }
 }
